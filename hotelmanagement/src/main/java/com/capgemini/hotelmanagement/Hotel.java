61110d224a726874cf11707e7f98fea8aa8cfe37
batista12 <50904472+batista12@users.noreply.github.com>
2020-10-24 08:01:39 -0700

    [Manasi] Welcome to the hotel reservation system.

---
+++ hotelmanagement/src/main/java/com/capgemini/hotelmanagement/Hotel.java
@@ -0,0 +1,13 @@
+package com.capgemini.hotelmanagement;
+
+/**
+ * 
+ *
+ */
+public class Hotel 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome" );
+    }
+}
---
+++ hotelmanagement/src/test/java/com/capgemini/hotelmanagement/HotelTest.java
@@ -0,0 +1,20 @@
+package com.capgemini.hotelmanagement;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * 
+ */
+public class HotelTest 
+{
+    /**
+     * 
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
